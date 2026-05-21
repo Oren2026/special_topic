60dfb4c4e847c9bf6753d4e82e873c6d24088eb1
Hermes Researcher <hermes-researcher@hermes>
2026-05-21 11:48:17 +0800

    fix(wpf_pos): OrderItem 實作 INotifyPropertyChanged讓 Quantity 改變時 UI 即時更新；+/X 按鈕加前景色確保可見

--- wpf_pos/Models/OrderItem.cs
+++ wpf_pos/Models/OrderItem.cs
@@ -1,8 +1,30 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace WPF_POS.Models;
 
-public class OrderItem
+public class OrderItem : INotifyPropertyChanged
 {
-    public Product Product { get; set; } = new();
-    public int Quantity { get; set; } = 1;
+    private int _quantity = 1;
+    public Product Product { get; set; } = new Product();
+
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (_quantity != value)
+            {
+                _quantity = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Subtotal));
+            }
+        }
+    }
+
     public decimal Subtotal => Product.Price * Quantity;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
